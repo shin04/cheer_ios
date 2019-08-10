b5a4f2eb6fd1d7c75c7d0111e75d52dc837874e5
shin04 <daikon0413@gmail.com>
2019-08-10 12:18:41 +0900

    Initial Commit

---
+++ cheer_ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  cheer_ios
+//
+//  Created by 梶原大進 on 2019/08/10.
+//  Copyright © 2019年 梶原大進. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ cheer_iosTests/cheer_iosTests.swift
@@ -0,0 +1,34 @@
+//
+//  cheer_iosTests.swift
+//  cheer_iosTests
+//
+//  Created by 梶原大進 on 2019/08/10.
+//  Copyright © 2019年 梶原大進. All rights reserved.
+//
+
+import XCTest
+@testable import cheer_ios
+
+class cheer_iosTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
